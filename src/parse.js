62f2353f625bb33769e6b633b7995e64cdd26c8a
devdigital <devdigital.cloud@gmail.com>
2018-04-23 17:58:19 +0100

    chore: wip

--- src/parse.js
+++ src/parse.js
@@ -1,7 +1,77 @@
-const parse = content => {
-  if (!content) {
+const $RefParser = require('json-schema-ref-parser')
+const isNil = require('inspected/schema/is-nil').default
+const isString = require('inspected/schema/is-string').default
+const isObject = require('inspected/schema/is-object').default
+const merge = require('deepmerge')
+
+const dereference = async (content, basePath, parser, resolver) => {
+  const options = {
+    parse: { custom: parser },
+    resolve: { file: false, http: false, custom: resolver },
+  }
+
+  if (basePath) {
+    console.log(content, basePath, parser, resolver)
+    return await $RefParser.dereference(basePath, content, options)
+  }
+
+  return await $RefParser.dereference(content, options)
+}
+
+const parse = async (content, options) => {
+  if (isNil(content)) {
     throw new Error('No content specified.')
+  }
+
+  if (!isString(content) && !isObject(content)) {
+    throw new Error('Content must be a string or spec object.')
+  }
+
+  // TODO: validate options against schema
+  if (!isNil(options) && !isObject(options)) {
+    throw new Error('Options should be an object.')
+  }
+
+  try {
+    const defaultOptions = {
+      parser: {
+        canParse: file => false,
+        parse: async file => {},
+      },
+      resolver: {
+        canResolve: file => false,
+        resolve: async file => {},
+      },
+    }
+
+    const compiledOptions = merge(defaultOptions, options)
+
+    const resolver = {
+      order: 1,
+      canRead: info =>
+        compiledOptions.resolver.canResolve({
+          path: info.url,
+          extension: info.extension,
+        }),
+      read: async info => {
+        return await compiledOptions.resolver.resolve({
+          path: info.url,
+          extension: info.extension,
+        })
+      },
+    }
+
+    const spec = await dereference(
+      content,
+      compiledOptions.basePath,
+      compiledOptions.parser,
+      compiledOptions.resolver
+    )
+
+    return spec
+  } catch (error) {
+    throw new Error(`There was an error parsing the specified spec:\n${error}`)
   }
 }
 
-export default parse
+module.exports = parse
---
+++ src/run.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const path = require('path')
+const parse = require('./parse')
+
+const fromFile = filePath => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      const spec = JSON.parse(data)
+      resolve(spec)
+    })
+  })
+}
+
+const toFile = (filePath, content) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, content, 'utf8', err => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve()
+    })
+  })
+}
+;(async () => {
+  try {
+    const spec = await fromFile(
+      path.resolve(
+        __dirname,
+        './__tests__/specs/v2.0/json/petstore-separate/spec/swagger.json'
+      )
+    )
+
+    const output = path.resolve(__dirname, './output.json')
+    const content = await parse(spec, {
+      basePath: process.cwd(),
+      resolver: {
+        canResolve: info => true,
+        resolve: async info => {
+          console.log(info.path)
+
+          return await fromFile(
+            path.resolve(
+              __dirname,
+              `./__tests__/specs/v2.0/json/petstore-separate/spec`,
+              info.path
+            )
+          )
+        },
+      },
+    })
+    await toFile(output, JSON.stringify(content, null, 2))
+    console.log('complete')
+  } catch (error) {
+    console.log(error)
+  }
+})()
