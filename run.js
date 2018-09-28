7ba9f63681f5801abe653845ecea0083ee90eb46
devdigital <devdigital.cloud@gmail.com>
2018-09-28 15:50:56 +0100

    chore: output all specs

--- run.js
+++ run.js
@@ -1,72 +1,72 @@
 const fs = require('fs')
+const fsExtra = require('fs-extra')
 const path = require('path')
 const parse = require('./src').default
 const yaml = require('js-yaml')
 
 const fromFile = parser => filePath => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-        return
-      }
-
-      const spec = parser(data)
-      resolve(spec)
-    })
-  })
+  const content = fs.readFileSync(filePath, 'utf8')
+  return parser(content)
 }
 
 const fromJsonFile = fromFile(data => JSON.parse(data))
 const fromYamlFile = fromFile(data => yaml.safeLoad(data))
 
 const toFile = (filePath, content) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, content, 'utf8', err => {
-      if (err) {
-        reject(err)
-        return
-      }
-
-      resolve()
-    })
-  })
+  fs.writeFileSync(filePath, content, { encoding: 'utf8', flag: 'w' })
 }
-;(async () => {
-  const basePath = './src/__tests__/specs/v3.0'
-  const spec = await fromYamlFile(
-    path.resolve(__dirname, basePath, './petstore.yaml')
-  )
 
-  const output = path.resolve(__dirname, './output.json')
+const outputSpec = async (basePath, filePath) => {
+  const parsedPath = path.parse(filePath)
+
+  let spec = null
+  if (parsedPath.ext === '.json') {
+    spec = fromJsonFile(filePath)
+  } else {
+    spec = fromYamlFile(filePath)
+  }
+
+  const versionFolder = filePath.split(path.sep).find(p => p.startsWith('v'))
+  const outputFolder = path.resolve(__dirname, './output', versionFolder)
+
+  fsExtra.ensureDirSync(outputFolder)
+
+  const outputPath = path.join(outputFolder, `${parsedPath.name}.json`)
+
   const content = await parse({
     basePath,
     dereference: true,
-    upgrade: {
-      enabled: true,
-      options: { patch: true, warnOnly: true },
-    },
-    parser: {
-      canParse: info => {
-        console.log('canParse info', info)
-        return false
-      },
-      parse: info => {
-        console.log('parse info', info)
-      },
-    },
+    // upgrade: {
+    //   enabled: true,
+    //   options: { patch: true, warnOnly: true },
+    // },
     resolver: {
-      canResolve: info => {
-        console.log('canResolve info', info)
-        return true
-      },
-      resolve: async info => {
-        console.log('resolve info', info)
-        return await fromFile(info.path)
-      },
+      canResolve: () => true,
+      resolve: info => Promise.resolve(fromJsonFile(info.path)),
     },
   })(spec)
 
-  await toFile(output, JSON.stringify(content, null, 2))
+  toFile(outputPath, JSON.stringify(content, null, 2))
+}
+
+const getFiles = folder => {
+  return fs
+    .readdirSync(folder)
+    .filter(file => fs.lstatSync(path.join(folder, file)).isFile())
+}
+
+const outputSpecs = async specsFolder => {
+  const files = getFiles(specsFolder)
+
+  files.forEach(async file => {
+    console.log(`outputting spec '${file}'...`)
+    await outputSpec(specsFolder, path.resolve(__dirname, specsFolder, file))
+    console.log(`spec file '${file}' successfully outputted.`)
+  })
+}
+;(async () => {
+  await outputSpecs('./src/specs/v2.0/json')
+  await outputSpecs('./src/specs/v3.0')
+
   console.log('complete')
 })()
