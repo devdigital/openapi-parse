3ff5083e22ba6ac3632c74d3acfbe62daf5ce007
devdigital <devdigital.cloud@gmail.com>
2018-10-02 11:14:44 +0100

    chore: update babel ignore glob

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   verbose: true,
   transform: { '^.+\\.js$': '<rootDir>/jest-preprocess.js' },
+  testRegex: '(/__tests__/.*\\.(jsx?)|(\\.|/)(test|spec))\\.(jsx?)$',
+  testPathIgnorePatterns: ['node_modules'],
   setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
 }
